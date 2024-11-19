7434bd996af3c8d50ea0d4adb9df22f6890dc594
Jarvist Moore Frost <jarvist@gmail.com>
2024-11-19 20:29:05 +0000

    Does Monte Carlo! Energies are clearly nonsense though.

--- src/IonMonteCarlo.jl
+++ src/IonMonteCarlo.jl
@@ -7,9 +7,9 @@ struct MCState
     N::Int64                    # Number of particles
     positions::Matrix{Float64}  # Nx3 matrix of particle positions
     charges::Vector{Float64}    # N-length vector of particle charges
-    L         # Lattice vectors / box size
-    beta::Float64             # 1/(kB*T)
-    σ::Float64                 # Surface charge density
+    L::Tuple{Float64,Float64,Float64} # Lattice vectors / box size
+    β::Float64                        # 1/(kB*T)
+    σ::Float64                         # Surface charge density
 end
 """
     MCState(charges::Vector{Float64}, L; T::Float64=300.0)
@@ -24,14 +24,14 @@ function MCState(charges::Vector, L; T::Float64=300.0)
     N = length(charges)
     # Initialize random positions within box
     positions = L .* rand(N, 3)' # scale all by scalar; or by individual (X,Y,Z)
-    # Calculate β = 1/(kB*T)
-    beta = 1.0 / (1.380649e-23 * T)  # kB in J/K
+    #  β = 1/(kB*T)
+    β = 1.0 / (1.380649e-23 * T)  # kB in J/K
 
     # Calculate surface charge density
     #  From Cahill V, 143 electrons over 50x50nm surface = 4% PS
-    σ = 143 * q / (50e-9 * 50e-9)
+    σ = 143 * q / (50nm * 50nm)
     
-    MCState(N, positions, charges, L, beta, σ)
+    MCState(N, positions, charges, L, β, σ)
 end
 
 function calc_energy(S::MCState)
@@ -42,6 +42,8 @@ function calc_energy(S::MCState)
         for j in (i+1):S.N # avoid double counting and self-interaction
             # Distance between i and j
             d=norm(S.positions[:,i] - S.positions[:,j])
+
+            # TODO: Should include replicas in X&Y for PBCs!
             
             # Calculate potential energy between pair
             #  ASSUMES WATER BETWEEN ALL IONS
@@ -62,5 +64,31 @@ function calc_energy(S::MCState)
     end
 
     return E
+end
+
+function mc_sweep!(S::MCState; δr=0.1nm)
+    ACCEPTED = 0
+    # One sweep = N attempted moves
+    for i in 1:S.N # should we shuffle?
+
+        # Store previous position and energy
+        r_prev = S.positions[:,i]
+        E_prev = calc_energy(S)
+        
+        # Trial move - randn displacement
+        S.positions[:,i] += δr * randn(3)
+        
+        # Calculate new energy
+        E_new = calc_energy(S)
+        
+        # Metropolis acceptance criterion
+        ΔE = E_new - E_prev
+        if ΔE < 0 || rand() < exp(-S.β * ΔE)
+            ACCEPTED += 1 # Accept move
+        else # Reject move & restore state
+            S.positions[:,i] = r_prev
+        end
+        println("Energy: $E_new (ΔE = $ΔE)")
+    end
 end
 
--- test/IonMonteCarlo.jl
+++ test/IonMonteCarlo.jl
@@ -3,3 +3,5 @@
 state = MembraneElectrostatics.MCState([1.0,1,1,-1,-1,-1], (10.0,10.0,10.0))
 
 MembraneElectrostatics.calc_energy(state) == 0.0
+
+MembraneElectrostatics.mc_sweep!(state)
